a57e2c50d40ebf247df95383177cb857086059ed
Cody Reandeau <codyreandeau@yahoo.com>
2016-07-25 23:35:34 -0400

    Added elements to the Doubly Linked List

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
   
   public static void main(String[] args) {
-  
-  }
+    
+    DLinkedList list = new DLinkedList();
+      
+      list.add("Doubly");
+      list.add("Linked");
+      list.add("List");
+    
+    }
 }
